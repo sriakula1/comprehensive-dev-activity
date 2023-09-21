97d94c6f2725ac128b8e55af62d2ad10e75459e4
Sri Akula <getintouchwithsri123@gmail.com>
2023-09-21 08:34:18 +0000

    Update project files
    Language: react
    Type: healthcare-platform
    Repository: brightpay/brighthub
    Year: 2023

--- activity/brightpay/react/module.jsx
+++ activity/brightpay/react/module.jsx
@@ -1,5 +1,5 @@
 // Healthcare Platform Module
-// Version 7
+// Version 8
 
 import React from 'react';
 
@@ -20,7 +20,7 @@ export const HealthcareModule = () => {
     return (
         <div>
             <h1>Healthcare Services</h1>
-            <p>Version 7</p>
+            <p>Version 8</p>
         </div>
     );
 };
