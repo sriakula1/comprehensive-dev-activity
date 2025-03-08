554ce43390d3d8af2d7b0b821981f8dba5963565
Sri Akula <getintouchwithsri123@gmail.com>
2025-03-08 08:08:33 +0000

    Enhance system
    Language: react
    Type: link-management
    Repository: brightpay/brightlink
    Month: 3/2025

--- activity/brightpay/react/changelog.md
+++ activity/brightpay/react/changelog.md
@@ -2508,3 +2508,14 @@
 
 ---
 
+# Brightpay Development - 2025-03-08
+
+**Commit #168**
+
+- Repository: brightpay/brightlink
+- Type: link-management
+- Language: react
+- Month: 3/2025
+
+---
+
--- activity/brightpay/react/module.tsx
+++ activity/brightpay/react/module.tsx
@@ -1,5 +1,5 @@
 // Link Management
-// Version 167
+// Version 168
 
 import React from 'react';
 
@@ -10,7 +10,7 @@ interface LinkmanagementProps {
 
 export const LinkmanagementComponent: React.FC<LinkmanagementProps> = ({
     config,
-    version = 167
+    version = 168
 }) => {
     const [data, setData] = React.useState(null);
 
