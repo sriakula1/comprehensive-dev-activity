3084a91a7d7008c67634ed0718e783ba687e39b6
Sri Akula <getintouchwithsri123@gmail.com>
2025-03-25 10:12:10 +0000

    Enhance system
    Language: react
    Type: web-application
    Repository: brightpay/realbio
    Month: 3/2025

--- activity/brightpay/react/module.tsx
+++ activity/brightpay/react/module.tsx
@@ -1,5 +1,5 @@
 // Web Application
-// Version 257
+// Version 258
 
 import React from 'react';
 
@@ -10,7 +10,7 @@ interface WebapplicationProps {
 
 export const WebapplicationComponent: React.FC<WebapplicationProps> = ({
     config,
-    version = 257
+    version = 258
 }) => {
     const [data, setData] = React.useState(null);
 
