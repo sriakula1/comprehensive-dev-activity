47bbe0def359bf3b5180a30c915037c9d4754ed3
Sri Akula <getintouchwithsri123@gmail.com>
2025-04-01 14:46:46 +0000

    Resolve issue
    Language: react
    Type: healthcare-platform
    Repository: brightpay/realhealth
    Month: 4/2025

--- activity/brightpay/react/module.tsx
+++ activity/brightpay/react/module.tsx
@@ -1,5 +1,5 @@
 // Healthcare Platform
-// Version 285
+// Version 286
 
 import React from 'react';
 
@@ -10,7 +10,7 @@ interface HealthcareplatformProps {
 
 export const HealthcareplatformComponent: React.FC<HealthcareplatformProps> = ({
     config,
-    version = 285
+    version = 286
 }) => {
     const [data, setData] = React.useState(null);
 
