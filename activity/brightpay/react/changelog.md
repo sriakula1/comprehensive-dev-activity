fd0b305419c2e30e956374fb7f7076237fe9e3a5
Sri Akula <getintouchwithsri123@gmail.com>
2025-04-11 07:27:36 +0000

    Add feature
    Language: react
    Type: healthcare-platform
    Repository: brightpay/realhealth
    Month: 4/2025

--- activity/brightpay/react/changelog.md
+++ activity/brightpay/react/changelog.md
@@ -3619,3 +3619,14 @@
 
 ---
 
+# Brightpay Development - 2025-04-11
+
+**Commit #321**
+
+- Repository: brightpay/realhealth
+- Type: healthcare-platform
+- Language: react
+- Month: 4/2025
+
+---
+
--- activity/brightpay/react/module.tsx
+++ activity/brightpay/react/module.tsx
@@ -1,5 +1,5 @@
 // Healthcare Platform
-// Version 320
+// Version 321
 
 import React from 'react';
 
@@ -10,7 +10,7 @@ interface HealthcareplatformProps {
 
 export const HealthcareplatformComponent: React.FC<HealthcareplatformProps> = ({
     config,
-    version = 320
+    version = 321
 }) => {
     const [data, setData] = React.useState(null);
 
