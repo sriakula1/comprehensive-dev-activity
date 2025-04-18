0f6957a5186bfeb770c47ac5a69d185e198c78fb
Sri Akula <getintouchwithsri123@gmail.com>
2025-04-18 07:27:51 +0000

    Create component
    Language: react
    Type: healthcare-platform
    Repository: brightpay/realhealth
    Month: 4/2025

--- activity/brightpay/react/module.tsx
+++ activity/brightpay/react/module.tsx
@@ -1,5 +1,5 @@
 // Healthcare Platform
-// Version 338
+// Version 339
 
 import React from 'react';
 
@@ -10,7 +10,7 @@ interface HealthcareplatformProps {
 
 export const HealthcareplatformComponent: React.FC<HealthcareplatformProps> = ({
     config,
-    version = 338
+    version = 339
 }) => {
     const [data, setData] = React.useState(null);
 
